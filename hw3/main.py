593cefddc8924a867a345a0093db47ce35535811
Adam Andersson <adander@kth.se>
2022-11-28 12:34:26 +0100

    Semi-working TriestBase

--- hw3/main.py
+++ hw3/main.py
@@ -1,5 +1,8 @@
 import random
 from collections import defaultdict
+from tqdm import tqdm
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 class Graph:
@@ -10,63 +13,92 @@ class Graph:
     def add_edge(self, u, v):
         self.adjacency_list[u].add(v)
         self.adjacency_list[v].add(u)
+
         self.all_edges.add((u, v))
-        self.all_edges.add((v, u))
 
     def remove_edge(self, u, v):
         self.adjacency_list[u].remove(v)
         self.adjacency_list[v].remove(u)
+
         self.all_edges.remove((u, v))
-        self.all_edges.remove((v, u))
-
-        if len(self.adjacency_list[u]) == 0:
-            del self.adjacency_list[u]
-        if len(self.adjacency_list[v]) == 0:
-            del self.adjacency_list[v]
 
 
-def flip_biased_coin(M, t):
-    probability = M/t
-    return random.random() < probability
+class TriestBase:
+    def __init__(self, M):
+        self.M = M
+        self.sample_graph = Graph()
+        self.tao = 0
+        self.t = 0
 
+    def calculate_xi(self):
+        nominator = self.t * (self.t - 1) * (self.t - 2)
+        denominator = self.M * (self.M - 1) * (self.M - 2)
 
-def sample_edge(sample_graph, timestep, M):
-    if timestep <= M:
-        return True
-    elif flip_biased_coin(M, timestep):
-        # in case of 'heads' on our biased coin flip
-        source_node, destination_node = random.choice(list(sample_graph.all_edges))
-        sample_graph.remove_edge(source_node, destination_node)
-        return True
-    return False
+        return max(1, nominator / denominator)
 
+    def estimate_global_triangles(self):
+        if self.t < self.M:
+            return self.tao
+        else:
+            return self.calculate_xi() * self.tao
 
-def triest_base(graph, t, M, u, v):
-    if sample_edge(graph, t, M):
-        graph.add_edge(u, v)
+    def update_counter(self, operation, u, v):
+        neighbourhood_uv = self.sample_graph.adjacency_list[u].intersection(self.sample_graph.adjacency_list[v])
+
+        for _ in range(len(neighbourhood_uv)):
+            if operation == '+':
+                self.tao += 1
+                print(self.tao, '+')
+            elif operation == '-':
+                self.tao -= 1
+                print(self.tao, '-')
+
+    def flip_biased_coin(self):
+        probability = self.M / self.t
+        return random.random() < probability
+
+    def sample_edge(self):
+        if self.t <= self.M:
+            return True
+        elif self.flip_biased_coin():
+            # in case of 'heads' on our biased coin flip
+            source_node, destination_node = random.choice(list(self.sample_graph.all_edges))
+            self.sample_graph.remove_edge(source_node, destination_node)
+            self.update_counter('-', source_node, destination_node)
+            return True
+        return False
+
+    def new_edge_from_stream(self, u, v):
+        self.t += 1
+        if self.sample_edge():
+            self.sample_graph.add_edge(u, v)
+            self.update_counter('+', u, v)
 
 
 if __name__ == "__main__":
     # --- CONSTANTS --- #
-    FILE_PATH = 'web-Google.txt'
-    NUMBER_OF_LINES_TO_READ = 1000
-    MAX_SET_SIZE = 30
+    FILE_PATH = 'web-Stanford.txt'
+    FILE_SPACE_SEP = '	'
+    FILE_COMMENT_CHAR = '#'
+    MAX_SET_SIZE = 12000
     # --- #
-    graph = Graph()
 
-    t = 0
-    with open(FILE_PATH, 'r') as f:
-        while t < NUMBER_OF_LINES_TO_READ:
-            line = f.readline()
+    seen_edges = set()
+    triest_base = TriestBase(MAX_SET_SIZE)
 
-            if line[0] == '#':
+    with open(FILE_PATH, 'r', encoding='utf-8') as f:
+        for line in tqdm(f):
+
+            if line[0] == FILE_COMMENT_CHAR:
                 # skip comments
                 continue
 
-            t += 1
-            src_node, dest_node = line.split('	')
-            int_src, int_dest = int(src_node), int(dest_node)
-            triest_base(graph, t, MAX_SET_SIZE, int_src, int_dest)
+            src_node, dest_node = sorted(list(map(int, line.split(FILE_SPACE_SEP))))
+            edge_tuple = (src_node, dest_node)
 
-    print(graph.all_edges)
-    print(graph.adjacency_list)
+            if edge_tuple not in seen_edges:
+                triest_base.new_edge_from_stream(src_node, dest_node)
+                seen_edges.add(edge_tuple)
+
+    global_triangles = triest_base.estimate_global_triangles()
+    print(f'In total, we found {int(global_triangles)} triangles')
